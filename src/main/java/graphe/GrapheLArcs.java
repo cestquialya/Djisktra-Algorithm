d625a58cf8cac74edc1b30700db401a9e57eabb9
alya1-yes <alyaayinde@gmail.com>
2024-04-11 11:50:10 +0200

    yeah

---
+++ src/main/java/graphe/GrapheLArcs.java
@@ -0,0 +1,4 @@
+package main.java.graphe;
+
+public class GrapheLArcs {
+}
--- src/test/java/graphe/IGrapheTest.java
+++ src/test/java/graphe/IGrapheTest.java
@@ -17,6 +17,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import main.java.graphe.GraphImporter;
+import main.java.graphe.IGraphe;
 import org.junit.jupiter.api.Test;
 
 class IGrapheTest {
