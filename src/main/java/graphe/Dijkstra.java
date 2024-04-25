21edc1048f33bb81e3c68eb70c0b222ad8e7d559
alya1-yes <alyaayinde@gmail.com>
2024-04-25 23:05:39 +0200

    Ajout de commentaire pour commencer Dijstrka

--- src/main/java/graphe/Dijkstra.java
+++ src/main/java/graphe/Dijkstra.java
@@ -11,7 +11,16 @@ public class Dijkstra {
     List<String> sommets= g.getSommets();
         dist.put(source,0);
         prev.put(source,"");
+        //regarder tt les succeseutr de source tant que ?
+        // peut etre tant que ya pas de succeseurs ou tant que ya pas de valuation + basse
+        //regarder tt les succceseurs et les valeur des arcs jusquau + petit il recommence la dest de larc le  plus petit je crois que ppoour repondre dcp au sommet qui a
+        // dist sommet valuation dans prev le sommet dans lequel on est il faudras lister tt le chemins ????? ttles destinations les plus courtes dans dist
+        for(int i=0 ; i< sommets.size();++i){
+            for(int j=0 ; j< sommets.size();++i){
+          if ( g.getSucc(sommets.get(i)).contains(sommets.get(j)) ){
+              prev.put(sommets.get(j),sommets.get(i) );
 
-
-
+          }
+        }
+    }
 }}
