743afe1d7af54d223ee8edd1d75b27239157704e
AYINDE Alya <alya.ayinde@iutseine.u-paris.domain>
2024-04-26 15:31:55 +0200

    Dijkstra algo debut

--- src/main/java/graphe/Dijkstra.java
+++ src/main/java/graphe/Dijkstra.java
@@ -11,12 +11,30 @@ public class Dijkstra {
     List<String> sommets= g.getSommets();
         dist.put(source,0);
         prev.put(source,"");
+
+        String pred= source;
+        String court= source;
+        for (String s: sommets) {
+            int min = g.getValuation(court, g.getSucc(court).get(0));
+            for (int i = 1; i < g.getSucc(court).size(); ++i) {
+                min = g.getValuation(court, g.getSucc(court).get(0));
+                if (g.getValuation(court, g.getSucc(court).get(i)) < min) {
+                    min = g.getValuation(court, g.getSucc(court).get(i));
+                    court = g.getSucc(court).get(i);
+                }
+            }
+            dist.put(court, min);
+            prev.put(court, pred); // Nv ancien
+        }
+        // on utilise prev pour contnuer les boucles
         //regarder tt les succeseutr de source tant que ?
         // peut etre tant que ya pas de succeseurs ou tant que ya pas de valuation + basse
         //regarder tt les succceseurs et les valeur des arcs jusquau + petit il recommence la dest de larc le  plus petit je crois que ppoour repondre dcp au sommet qui a
         // dist sommet valuation dans prev le sommet dans lequel on est il faudras lister tt le chemins ????? ttles destinations les plus courtes dans dist
+
         for(int i=0 ; i< sommets.size();++i){
             for(int j=0 ; j< sommets.size();++i){
+
           if ( g.getSucc(sommets.get(i)).contains(sommets.get(j)) ){
               prev.put(sommets.get(j),sommets.get(i) );
 
--- src/test/java/graphe/IGrapheTest.java
+++ src/test/java/graphe/IGrapheTest.java
@@ -171,7 +171,7 @@ class IGrapheTest {
 				Path file1 = iterator1.next();
 				Path file2 = iterator2.next();
 
-				IGraphe g = new GrapheHHAdj();
+				IGraphe g = new GrapheLAdj();
 				Arc arc = GraphImporter.importer(file1.toFile(), g);
 
 				List<Integer> listeEntiers = new ArrayList<>();
